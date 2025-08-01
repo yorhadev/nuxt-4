6cb3cdfbeb2ca5da05e8d3fb183c56c10ffcc28e
yorhadev <rafael.takano.dev@gmail>
2025-08-01 16:38:02 -0300

    chore(eslint): installed eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
   css: ["~/assets/css/main.css"],
 });
